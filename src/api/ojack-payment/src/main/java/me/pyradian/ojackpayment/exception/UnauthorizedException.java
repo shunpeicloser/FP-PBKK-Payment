82df166e0cc5b2fb9a5f1326f29526bb3422190c
Rahadian <rahadian.k.putra@gmail.com>
2019-05-11 01:01:54 +0700

    fixed UnauthorizedException should inherit RuntimeException

--- src/api/ojack-payment/src/main/java/me/pyradian/ojackpayment/exception/UnauthorizedException.java
+++ src/api/ojack-payment/src/main/java/me/pyradian/ojackpayment/exception/UnauthorizedException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UnauthorizedException extends Exception {
+public class UnauthorizedException extends RuntimeException {
     public UnauthorizedException(String message) { super(message); }
 }
