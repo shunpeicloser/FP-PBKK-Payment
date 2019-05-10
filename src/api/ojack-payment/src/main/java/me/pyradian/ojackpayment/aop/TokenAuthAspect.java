b165ceb02c4f5b14cc81e4fc64bb492c86df63a6
Rahadian <rahadian.k.putra@gmail.com>
2019-05-11 02:41:56 +0700

    token checking for USER now supports comma-separated list

--- src/api/ojack-payment/src/main/java/me/pyradian/ojackpayment/aop/TokenAuthAspect.java
+++ src/api/ojack-payment/src/main/java/me/pyradian/ojackpayment/aop/TokenAuthAspect.java
@@ -10,6 +10,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Aspect
 @Component
@@ -27,7 +28,7 @@ public class TokenAuthAspect {
         HttpServletRequest request = requestAttributes.getRequest();
 
         String authHeader = request.getHeader("Authorization");
-        System.out.println(authHeader);
+//        System.out.println(authHeader);
         Claims claims = jwtService.getBody(authHeader);
 
         // if not strict, ADMIN and USER can have the access
@@ -41,8 +42,14 @@ public class TokenAuthAspect {
         }
 
         // get token account_type if not admin
-        if ( tokenAuth.auth_role().equals("USER") && !tokenAuth.account_type().equals(claims.get("atp"))) {
-            throw new UnauthorizedException("Unauthorized bro!");
+        if ( tokenAuth.auth_role().equals("USER") ) {
+            String[] types = tokenAuth.account_type().split(",");
+            boolean exists = false;
+            for(String type: types) {
+                System.out.println(type);
+                if (type.equals(claims.get("atp"))) exists = true;
+            }
+            if (!exists) throw new UnauthorizedException("Unauthorized bro!");
         }
     }
 }
