c4eed86a4a563b6d65972807e1b9304bdef3ba1d
Rahadian <rahadian.k.putra@gmail.com>
2019-05-11 15:42:29 +0700

    food order bug fix and major refactor

--- src/api/ojack-payment/src/main/java/me/pyradian/ojackpayment/controller/FoodOrderController.java
+++ src/api/ojack-payment/src/main/java/me/pyradian/ojackpayment/controller/FoodOrderController.java
@@ -1,7 +1,10 @@
 package me.pyradian.ojackpayment.controller;
 
+import io.jsonwebtoken.Claims;
+import me.pyradian.ojackpayment.aop.TokenAuth;
 import me.pyradian.ojackpayment.exception.BadRequestException;
 import me.pyradian.ojackpayment.exception.NotFoundException;
+import me.pyradian.ojackpayment.exception.UnauthorizedException;
 import me.pyradian.ojackpayment.model.FoodOrder;
 import me.pyradian.ojackpayment.model.Wallet;
 import me.pyradian.ojackpayment.repository.FoodOrderRepository;
@@ -19,95 +22,119 @@ import java.util.List;
 public class FoodOrderController {
     public static final String BASE_URL = "/api/v1/transaction/foodorder";
 
-    private FoodOrderRepository foodOrderRepository;
-    private WalletRepository walletRepository;
     private FoodOrderService foodOrderService;
 
-    public FoodOrderController(FoodOrderRepository foodOrderRepository, WalletRepository walletRepository,
-                               FoodOrderService foodOrderService) {
-        this.foodOrderRepository = foodOrderRepository;
-        this.walletRepository = walletRepository;
+    public FoodOrderController(FoodOrderService foodOrderService) {
         this.foodOrderService = foodOrderService;
     }
 
+    @TokenAuth(strict = false)
     @GetMapping
-    public List<FoodOrder> getAllFoodOrder() {
-        return this.foodOrderRepository.findAll();
+    public List<FoodOrder> getAllFoodOrder(@RequestHeader("Authorization") String token) {
+        Claims claims = foodOrderService.getClaims(token);
+
+        if (claims.get("rol").equals("ADMIN"))
+            return foodOrderService.getFoodOrderRepository().findAll();
+
+        return foodOrderService.getFoodOrderRepository().findByCustomerWalletNumber(claims.getSubject());
     }
 
+    @TokenAuth
     @PostMapping
     public ResponseEntity<FoodOrder> requestFoodOrder(@RequestBody FoodOrder fo) {
-        int res = this.foodOrderService.isValid(fo);
+        this.foodOrderService.isValid(fo);
 
-        if (res == -1)
-            throw new BadRequestException("Some or all fields are empty");
-        else if (res == -2)
-            throw new BadRequestException("Invalid bill received. Must be > 0");
-        else if (res == -3)
-            throw new BadRequestException("Some or all wallets credential are missing");
-        else if (res == -4)
-            throw new BadRequestException("Customer wallet balance is insufficient to complete the order");
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Location", FoodOrderController.BASE_URL + "/" + fo.getTransactionId());
-        Wallet customerWallet = this.walletRepository.findByWalletNumber(fo.getCustomerWalletNumber());
+        Wallet customerWallet = foodOrderService.getWalletRepository().findByWalletNumber(fo.getCustomerWalletNumber());
         customerWallet.setBalance(customerWallet.getBalance() - fo.getFoodOrderBill());
-        this.foodOrderRepository.insert(fo);
-        this.walletRepository.save(customerWallet);
+        foodOrderService.getFoodOrderRepository().insert(fo);
+        foodOrderService.getWalletRepository().save(customerWallet);
 
         return new ResponseEntity<>(fo, headers, HttpStatus.CREATED);
     }
 
+    @TokenAuth(strict = false)
     @GetMapping("/{foodOrderId}")
-    public FoodOrder getFoodOrder(@PathVariable("foodOrderId") String foodOrderId) {
-        FoodOrder fo = this.foodOrderRepository.findByTransactionId(foodOrderId);
+    public FoodOrder getFoodOrder(@PathVariable("foodOrderId") String foodOrderId,
+                                  @RequestHeader("Authorization") String token) {
+        Claims claims = foodOrderService.getClaims(token);
+
+        FoodOrder fo = foodOrderService.getFoodOrderRepository().findByTransactionId(foodOrderId);
         if (fo == null)
             throw new NotFoundException("Food Order with ID " + foodOrderId + " is non-existent");
+
+        // admin can see all trx
+        if (claims.get("rol").equals("ADMIN")) {
+            return fo;
+        }
+
+        if (!foodOrderService.canCheckOrder(fo, claims.getSubject()))
+            throw new UnauthorizedException("You cannot view this transaction");
 
         return fo;
     }
 
+    @TokenAuth(auth_role = "USER", account_type = "driver")
     @PatchMapping("/confirm/{foodOrderId}")
-    public FoodOrder confirmFoodOrder(@PathVariable("foodOrderId") String foodOrderId) {
-        FoodOrder fo = this.foodOrderRepository.findByTransactionId(foodOrderId);
+    public FoodOrder confirmFoodOrder(@PathVariable("foodOrderId") String foodOrderId,
+                                      @RequestHeader("Authorization") String token) {
+        Claims claims = foodOrderService.getClaims(token);
+        FoodOrder fo = foodOrderService.getFoodOrderRepository().findByTransactionId(foodOrderId);
 
+        // non-existent order
         if (fo == null)
             throw new NotFoundException("Food Order with ID " + foodOrderId + " is non-existent");
 
+        // attempt to confirm a canceled or confirmed order
         if (!this.foodOrderService.isPending(fo))
             throw new BadRequestException("Food Order with ID " + foodOrderId + " is already confirmed/canceled");
 
+        // only driver in foodorder can confirm
+        if (!foodOrderService.canCheckOrder(fo, claims.getSubject()))
+            throw new UnauthorizedException("The driver is not in charge delivering the food, thus cannot confirm this order");
+
         // fetch driver and restaurant wallet, then add fund
-        Wallet driverWallet = this.walletRepository.findByWalletNumber(fo.getDriverWalletNumber());
-        Wallet restaurantWallet = this.walletRepository.findByWalletNumber(fo.getRestaurantWalletNumber());
+        Wallet driverWallet = foodOrderService.getWalletRepository().findByWalletNumber(fo.getDriverWalletNumber());
+        Wallet restaurantWallet = foodOrderService.getWalletRepository().findByWalletNumber(fo.getRestaurantWalletNumber());
         driverWallet.setBalance(driverWallet.getBalance() + fo.getDriverAmount());
         restaurantWallet.setBalance(restaurantWallet.getBalance() + fo.getRestaurantAmount());
-        this.walletRepository.save(driverWallet);
-        this.walletRepository.save(restaurantWallet);
+        foodOrderService.getWalletRepository().save(driverWallet);
+        foodOrderService.getWalletRepository().save(restaurantWallet);
 
         fo.setStatus("confirmed");
-        this.foodOrderRepository.save(fo);
+        foodOrderService.getFoodOrderRepository().save(fo);
 
         return fo;
     }
 
+    @TokenAuth(auth_role = "USER", account_type = "customer,restaurant")
     @PatchMapping("/cancel/{foodOrderId}")
-    public FoodOrder cancelFoodOrder(@PathVariable("foodOrderId") String foodOrderId) {
-        FoodOrder fo = this.foodOrderRepository.findByTransactionId(foodOrderId);
+    public FoodOrder cancelFoodOrder(@PathVariable("foodOrderId") String foodOrderId,
+                                     @RequestHeader("Authorization") String token) {
+        Claims claims = foodOrderService.getClaims(token);
+        FoodOrder fo = foodOrderService.getFoodOrderRepository().findByTransactionId(foodOrderId);
 
+        // non-existent order
         if (fo == null)
             throw new NotFoundException("Food Order with ID " + foodOrderId + " is non-existent");
 
+        // attempt to cancel a canceled or confirmed order
         if (!this.foodOrderService.isPending(fo))
             throw new BadRequestException("Food Order with ID " + foodOrderId + " is already confirmed/cancel");
 
+        // only customer or restaurnat in foodorder can cancel
+        if (!foodOrderService.canCheckOrder(fo, claims.getSubject()))
+            throw new UnauthorizedException("Current wallet number is not in this order, thus cannot cancel the order");
+
         // fetch customer wallet and then refund
-        Wallet customerWallet = this.walletRepository.findByWalletNumber(fo.getCustomerWalletNumber());
+        Wallet customerWallet = foodOrderService.getWalletRepository().findByWalletNumber(fo.getCustomerWalletNumber());
         customerWallet.setBalance(customerWallet.getBalance() + fo.getFoodOrderBill());
-        this.walletRepository.save(customerWallet);
+        foodOrderService.getWalletRepository().save(customerWallet);
 
         fo.setStatus("canceled");
-        this.foodOrderRepository.save(fo);
+        foodOrderService.getFoodOrderRepository().save(fo);
 
         return fo;
     }
