2a7f6ca3382f4f77b0fe1ee0ec9ef0fe7b730cf2
taufiq <tirtajiwanggataufiq@gmail.com>
2019-05-18 23:01:57 +0700

    up

--- ui/test.php
+++ ui/test.php
@@ -1,6 +1,8 @@
 <?php
     require_once("jwtutils.php");
-    $secretkey = "EmYTCuhxT3\$FpXUVXDj*f0e4LbcpDAk^apes1eAuUtUrC3V%1XPSGD^2KPj*u^L&";
+    $secretkey = 'EmYTCuhxT3$FpXUVXDj*f0e4LbcpDAk^apes1eAuUtUrC3V%1XPSGD^2KPj*u^L&';
+    $secretkey = base64_decode($secretkey);
+    // echo $secretkey;
     $baseURL = "https://pyradian.me:9443";
     $serviceURL = "/api/v1/wallet/6288804862379";
     $payload = json_encode(['sub'=>'6288804862379', 'name'=>'Alcredo', 'rol'=>'USER', 'atp'=>'customer']);
