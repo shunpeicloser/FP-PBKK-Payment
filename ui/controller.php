4db9273c6a4de0058dd43ec853cc05175dbb66fc
taufiq <tirtajiwanggataufiq@gmail.com>
2019-05-21 01:38:23 +0700

    linked to customer

--- ui/controller.php
+++ ui/controller.php
@@ -4,29 +4,37 @@
     $formname = $_POST["formname"];
 
     if($formname == "login"){
-        $handle= fopen("user.file", "r");
-        $fusername;
-        $fnohp;
-        $frole;
-        for($i=0;($line = fgets($handle));$i++){
-            list($fusername[$i],$fnohp[$i],$frole[$i])=preg_split('/ /',$line);
-            $frole[$i] = substr($frole[$i],0,-1);
-        }
-        $username = $_POST["username"];
-        $password = $_POST["password"];
+        // $handle= fopen("user.file", "r");
+        // $fusername;
+        // $fnohp;
+        // $frole;
+        // for($i=0;($line = fgets($handle));$i++){
+        //     list($fusername[$i],$fnohp[$i],$frole[$i])=preg_split('/ /',$line);
+        //     $frole[$i] = substr($frole[$i],0,-1);
+        // }
+        // $username = $_POST["username"];
+        // $password = $_POST["password"];
+
         $status = "accepted"; // user auth here
         if($status == "accepted"){
             $get=0;
-            for($i=0;$i<sizeof($fusername);$i++){
-                if(strcmp($fusername[$i],$username)==0){
-                    $get=1;
-                    $_SESSION['username']=$fusername[$i];
-                    $_SESSION['nohp']=$fnohp[$i];
-                    $_SESSION['role']=$frole[$i];
-                    break;
-                }
+            // for($i=0;$i<sizeof($fusername);$i++){
+            //     if(strcmp($fusername[$i],$username)==0){
+            //         $get=1;
+            //         $_SESSION['username']=$fusername[$i];
+            //         $_SESSION['nohp']=$fnohp[$i];
+            //         $_SESSION['role']=$frole[$i];
+            //         break;
+            //     }
+            // }
+            $get = callCustomerAPI(base64_encode("payment:tjfinance"), $_POST['username'], $_POST['password']);
+            // var_dump($get); die();
+            if($get!=0){
+                $_SESSION['username']=$_POST['username'];
+                $_SESSION['nohp']=$get[0];
+                $_SESSION['role']=$get[1];
+                header("Location: home.php");
             }
-            if($get==1) header("Location: home.php");
             else{
                 $_SESSION['error'] = "<br/><div id='errormsg' style='border: 2px solid black; width: 20vw; background-color: red;'>Username/password tidak valid</div>";
                 header("Location: index.php");
--- ui/jwtutils.php
+++ ui/jwtutils.php
@@ -30,31 +30,81 @@
                     'content' => $body
                 )
             );
-            // $request = new HttpRequest();
-            // $request->setUrl($baseurl.$serviceurl);
-            // $request->setMethod($method);
-
-            // $request->setHeaders(array(
-            //     'Authorization' => "Bearer $jwt",
-            //     'Content-Type' => 'application/json'
-            // ));
-
-            // $request->setBody($body);
-
-            // try {
-            //     $response = $request->send();
-
-            //     echo $response->getBody();
-            //     die();
-            // } catch (HttpException $ex) {
-            //     echo $ex;
-            // }
-            // return $response->getBody();
         }
         $context = stream_context_create($opt);
         $returned_data = file_get_contents($baseurl.$serviceurl, false, $context);
         // var_dump($returned_data);
         return $returned_data;
+    }
+    // statis pake x-www-form-urlencoded
+    function callCustomerAPI($basicval, $username, $password){
+        $usertype = ["user", "driver", "restaurant"];
+        $opt;
+        $url = 'https://rendoru.com/kuliah/pbkk/oauth/token';
+
+        foreach($usertype as $prefix){
+            $body = http_build_query(array(
+                'grant_type' => 'password',
+                'username' => $prefix . "_" . $username,
+                'password' => $password
+            ));
+            // echo $body; die();
+            $opt = array(
+                'http' => array(
+                    'method' => "POST",
+                    'header' => "Authorization: Basic $basicval\r\nContent-Type: application/x-www-form-urlencoded\r\n",
+                    'content' => $body
+                )
+            );
+            
+            $context = stream_context_create($opt);
+            $returned_data = file_get_contents($url, false, $context);
+            if($returned_data != false){
+                break;
+            }
+        }
+        if($returned_data == false){
+            return 0;
+        }
+        $jwt = json_decode($returned_data);
+        $jwt = $jwt->access_token;
+        $url = 'https://rendoru.com/kuliah/pbkk/oauth/check_token';
+
+        $body = http_build_query(array(
+            'token' => $jwt
+        ));
+
+        $opt = array(
+            'http' => array(
+                'method' => "POST",
+                'header' => "Authorization: Bearer $jwt\r\nContent-Type: application/x-www-form-urlencoded\r\n",
+                'content' => $body
+            )
+        );
+
+        $context = stream_context_create($opt);
+        $returned_data = file_get_contents($url, false, $context);
+        $returned_data = json_decode($returned_data, true);
+        // $tmp = $returned_data['sub'];
+        // var_dump(implode(",", $returned_data)); die();
+        $tmp = "$returned_data[sub]";
+        // $tmp = (string)$tmp;
+        $url = "https://rendoru.com/kuliah/pbkk/users/".$tmp;
+        // echo $url; die();
+        $opt = array(
+            'http' => array(
+                'method' => "GET",
+                'header' => "Authorization: Bearer $jwt\r\n"
+            )
+        );
+        $context = stream_context_create($opt);
+        $returned_data = file_get_contents($url, false, $context);
+        $returned_data = json_decode($returned_data, true);
+        // var_dump($returned_data); die();
+        $ret1 = "$returned_data[no_handphone]";
+        $ret2 = "$returned_data[identifier]";
+        // var_dump($ret); die();
+        return [$ret1, $ret2];
     }
 
 ?>
